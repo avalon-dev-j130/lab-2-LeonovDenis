e8c9ba52efa02c1e19404061d9e9359f2dfa23f1
LeonovDenis <89516446585@yandex.ru>
2019-12-27 16:37:46 +0300

    ready1

--- src/ru/avalon/java/j30/labs/Main.java
+++ src/ru/avalon/java/j30/labs/Main.java
@@ -1,6 +1,11 @@
 package ru.avalon.java.j30.labs;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Collection;
 import java.util.Properties;
@@ -8,9 +13,10 @@ import java.util.Properties;
 /**
  * Лабораторная работа №3
  * <p>
- * Курс: "DEV-OCPJP. Подготовка к сдаче сертификационных экзаменов серии Oracle Certified Professional Java Programmer"
+ * Курс: "DEV-OCPJP. Подготовка к сдаче сертификационных экзаменов серии Oracle
+ * Certified Professional Java Programmer"
  * <p>
- * Тема: "JDBC - Java Database Connectivity" 
+ * Тема: "JDBC - Java Database Connectivity"
  *
  * @author Daniel Alpatov <danial.alpatov@gmail.com>
  */
@@ -18,13 +24,14 @@ public class Main {
 
     /**
      * Точка входа в приложение
-     * 
+     *
      * @param args the command line arguments
      */
     public static void main(String[] args) throws SQLException {
         /*
          * TODO #01 Подключите к проекту все библиотеки, необходимые для соединения с СУБД.
          */
+
         try (Connection connection = getConnection()) {
             ProductCode code = new ProductCode("MO", 'N', "Movies");
             code.save(connection);
@@ -38,52 +45,75 @@ public class Main {
          * TODO #14 Средствами отладчика проверьте корректность работы программы
          */
     }
+
     /**
      * Выводит в кодсоль все коды товаров
-     * 
+     *
      * @param connection действительное соединение с базой данных
-     * @throws SQLException 
-     */    
+     * @throws SQLException
+     */
     private static void printAllCodes(Connection connection) throws SQLException {
         Collection<ProductCode> codes = ProductCode.all(connection);
         for (ProductCode code : codes) {
             System.out.println(code);
         }
     }
+
     /**
      * Возвращает URL, описывающий месторасположение базы данных
-     * 
+     *
      * @return URL в виде объекта класса {@link String}
      */
     private static String getUrl() {
         /*
          * TODO #02 Реализуйте метод getUrl
          */
-        throw new UnsupportedOperationException("Not implemented yet!");
+        String name = "sample";
+
+        return "jdbc:derby://localhost:1527/" + name;
     }
+
     /**
      * Возвращает параметры соединения
-     * 
-     * @return Объект класса {@link Properties}, содержащий параметры user и 
+     *
+     * @return Объект класса {@link Properties}, содержащий параметры user и
      * password
      */
     private static Properties getProperties() {
         /*
          * TODO #03 Реализуйте метод getProperties
          */
-        throw new UnsupportedOperationException("Not implemented yet!");
+        String pathroot=Thread.currentThread().getContextClassLoader()
+                                              .getResource("").getPath();
+        String path = pathroot+"resource/url.properties";
+        Properties prop = new Properties();
+        try (InputStream fis = new FileInputStream(path)) {
+
+            prop.load(fis); 
+        } catch (FileNotFoundException e) {
+            System.err.println("Properties not load");
+        } catch (IOException e) {
+            System.err.println("Properties not found");
+        }
+        
+        return prop;
     }
+
     /**
      * Возвращает соединение с базой данных Sample
-     * 
+     *
      * @return объект типа {@link Connection}
-     * @throws SQLException 
+     * @throws SQLException
      */
     private static Connection getConnection() throws SQLException {
         /*
          * TODO #04 Реализуйте метод getConnection
          */
-        throw new UnsupportedOperationException("Not implemented yet!");
+        String url=getUrl();
+        String user=getProperties().getProperty("db.user");
+        String password=getProperties().getProperty("db.password");
+        
+        return DriverManager.getConnection(url, user, password);
     }
-    
+
 }
